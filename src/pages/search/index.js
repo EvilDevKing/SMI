59998c26241d650a1f2d4c5db062b3adb103ef9c
Angel Rodriguez <buznetconsult19@outlook.com>
2024-05-27 16:54:30 -0400

    update

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -104,7 +104,7 @@ const SearchPage = () => {
     return (
         <div className="page-search d-flex flex-column align-items-center">
             <p className="w-100 fs-20 fc-primary f-bold text-center">Suggested Methods Improvement (SMI)</p>
-            <p className="f-bold fs-20 fc-primary text-center mt-20 title-border py-1">Search</p>
+            <p className="w-100 f-bold fs-20 fc-primary text-center mt-20 title-border py-1">Search</p>
             <form className="search-form px-10">
                 <div className="form-section mt-20">
                     <input type="number" min={1} max={99999} className="input-control fs-18 bordered w-100 px-10 py-2" placeholder="SMI Number" />
